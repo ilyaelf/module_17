80a66d3a1afc1d190a5a25e750a49ff4a4087f08
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-11-04 20:43:38 +0300

    Update task.py

--- app/models/task.py
+++ app/models/task.py
@@ -5,7 +5,6 @@ from app.models import *
 
 class Task(Base):
     __tablename__ = "tasks"
-    __table_args__ = {'keep_existing':True}
     id = Column(Integer,primary_key=True,index=True)
     title = Column(String)
     content = Column(String)
@@ -15,6 +14,3 @@ class Task(Base):
     slug = Column(String,unique=True,index=True)
 
     user = relationship('User',back_populates='tasks')
-
-#from sqlalchemy.schema import CreateTable
-#print(CreateTable(Task.__table__))
