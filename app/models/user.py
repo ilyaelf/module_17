7bc3e208c058153659b5a8f630c303af566fff34
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-11-04 20:44:51 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -5,15 +5,11 @@ from app.models import *
 
 class User(Base):
     __tablename__ = "users"
-    __table_args__ = {'keep_existing':True}
     id = Column(Integer,primary_key=True,index=True)
     username = Column(String)
     firstname = Column(String)
     lastname = Column(String)
     age = Column(Integer)
     slug = Column(String,unique=True,index=True)
-
+    
     tasks = relationship("Task",back_populates="user")
-
-#from sqlalchemy.schema import CreateTable
-#print(CreateTable(User.__table__))
